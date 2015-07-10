0cdc9de487a05324a8590103d32a9e77b5fdfeda
Katie Stamm <katie@Katies-MacBook-Pro.local>
2015-07-09 21:52:38 -0600

    added css rating

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).on('ready', function() {
   
 var AllQuotes = []
-var quoteId = 1;
+var quoteId = 0;
 
 var Quote = function(quote, author){
 	this.quote 	= quote;
@@ -24,7 +24,7 @@ var Quote = function(quote, author){
 
 		var setQuote = new Quote(q, a);
 
-		var $quoteBlock = $('<div class="quote-block" data-id="' + setQuote.id + '"><p class="quote-text"></p><p class="author"></p><i class="rate fa fa-star fa-lg" data-rating="1"></i><i class="rate fa fa-star fa-lg" data-rating="2"></i><i class="rate fa fa-star fa-lg" data-rating="3"></i><i class="rate fa fa-star fa-lg" data-rating="4"></i><i class="rate fa fa-star fa-lg" data-rating="5"></i><a href="#" class="remove">remove this quote</a></div>');
+		var $quoteBlock = $('<div class="quote-block" data-id="' + setQuote.id + '"><p class="quote-text"></p><p class="author"></p><div class="rating"><span class="star rate" data-rating="1"></span><span class="star rate" data-rating="2"></span><span class="star rate" data-rating="3"></span><span class="star rate" data-rating="4"></span><span class="star rate" data-rating="5"></span></div><a href="#" class="remove">remove this quote</a></div>');
 
 		$('input').val('');
 		$('.container').append($quoteBlock);
@@ -62,33 +62,40 @@ var Quote = function(quote, author){
 	});
 
 	// rating
+
+	$(document).on('mouseover', '.rate', function(){
+		$(this).prevAll('.rate').andSelf().addClass('rate-active');
+	})
+
+	$(document).on('mouseout', '.rate', function(){
+		$(this).nextAll().andSelf().removeClass('rate-active');
+	})
+
 	$(document).on('click', '.rate', function(){
+		
 
-		var $rating = this.getAttribute('data-rating'); //get attr value of data-id from star icon
+		var $rating = parseInt(this.getAttribute('data-rating')); //get attr value of data-id from star icon
 		var dataID = $(this).closest('.quote-block').data('id'); // figure out which quote the user is rating by key property id
+		console.log( (AllQuotes[dataID]).rating );
 
-		// set the rating property value to $rating
+		(AllQuotes[dataID]).rating = $rating; // set prop key to value of data-rating attr
+		// console.log('rating prop: ', (AllQuotes[dataID]).rating);
+		
 
-		// var result = _.find(AllQuotes, dataID)
-		console.log('var rating: ', $rating)
-		console.log('block id: ', dataID) // I'll use this later to do a "random quote"
+		// console.log('var $rating: ', $rating)
+		// console.log('block id: ', dataID) // I'll use this later to do a "random quote"
 
 		var result = AllQuotes.sort(function(a, b) { // sort the object in order of rating
 			return a.rating - b.rating
 		});
 
-		console.log(AllQuotes);
+		$(this).andSelf().prevAll('.rate').addClass(???) // not sure how to add complex css selector here
 
+
+	console.log(AllQuotes);
 	});
+	
 
-
-	$(document).on('mouseover', '.rate', function(){
-		$(this).prevAll('.rate').andSelf().addClass('rate-hover');
-	})
-
-	$(document).on('mouseleave', '.rate', function(){
-		$(this).nextAll().andSelf().removeClass('rate-hover');
-	})
 
 
 
